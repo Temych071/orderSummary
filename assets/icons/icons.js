d27dd5e03841bedd09717765600c1a1713b5bafa
Goto.07 <goto.07@mail.ru>
2021-09-14 15:22:46 +0300

    first commit

---
+++ assets/icons/icons.js
@@ -0,0 +1,18 @@
+// Пример подключения иконки
+// import './svg/my-icon.svg';
+
+import './svg/alert.svg';
+import './svg/comment.svg';
+import './svg/eye.svg';
+import './svg/heart.svg';
+import './svg/info.svg';
+import './svg/instagram.svg';
+import './svg/more.svg';
+import './svg/qsoft_logo.svg';
+import './svg/question.svg';
+import './svg/search.svg';
+import './svg/star.svg';
+import './svg/youtube.svg';
+
+import './svg/face-icon.svg';
+
